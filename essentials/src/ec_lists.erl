7d7f955404bc1709220257fc11cf05f6057ee3d1
Josh Adams <josh@isotope11.com>
2013-10-11 11:47:59 -0500

    My failing version of ec_lists

---
+++ essentials/src/ec_lists.erl
@@ -0,0 +1,31 @@
+%%% @author Josh Adams
+%%% @doc manipulates lists
+%%% @copyright 2013 PUBLIC DOMAIN GEEBUS
+
+-module(ec_lists).
+-export([remove_odd/1]).
+
+%% @doc remove odd elements form a list of integers
+-spec remove_odd(list()) -> list().
+remove_odd(List) ->
+    remove_odd(List, []).
+
+-spec remove_odd(list(), list()) -> list().
+remove_odd([], Acc) ->
+    Acc;
+remove_odd([H|T], Acc) when H rem 2 == 0 ->
+    remove_odd(T, [H|Acc]);
+remove_odd([_H,T], Acc) ->
+    remove_odd(T, Acc).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Test Descriptions %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%
+
+remove_odd_test() ->
+    ?assertEqual([], remove_odd([])),
+    ?assertEqual([2,4], remove_odd([1,2,3,4,5])).
+-endif.
