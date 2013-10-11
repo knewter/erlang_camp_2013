8772f491fbf2b74a8f8b700d79628125aa96447e
Josh Adams <josh@isotope11.com>
2013-10-11 10:42:45 -0500

    Add sum_seq

--- essentials/src/ec_recurecursion.erl
+++ essentials/src/ec_recurecursion.erl
@@ -3,7 +3,7 @@
 %%% @copyright 2013 PUBLIC DOMAIN GEEBUS
 
 -module(ec_recurecursion).
--export([loop/1]).
+-export([loop/1, sum_seq/1]).
 
 %% @doc loop a bit to recur...
 -spec loop(integer()) -> ok.
@@ -11,6 +11,11 @@ loop(0) ->
     ok;
 loop(N) ->
     loop(N-1).
+
+%% doc add stuff sequentially
+-spec sum_seq(integer()) -> integer().
+sum_seq(0) -> 0;
+sum_seq(N) -> N + sum_seq(N - 1).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -22,4 +27,8 @@ loop(N) ->
 loop_test() ->
     ?assertEqual(ok, loop(0)),
     ?assertEqual(ok, loop(3)).
+
+sum_seq_test() ->
+    ?assertEqual(sum_seq(0), 0),
+    ?assertEqual(sum_seq(3), 6).
 -endif.
