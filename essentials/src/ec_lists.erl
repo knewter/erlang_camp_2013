5098a34b0fa5a130d2f30f48e1bd2abe2d1d7d6a
Josh Adams <josh@isotope11.com>
2013-10-11 11:48:28 -0500

    The working version

--- essentials/src/ec_lists.erl
+++ essentials/src/ec_lists.erl
@@ -8,15 +8,15 @@
 %% @doc remove odd elements form a list of integers
 -spec remove_odd(list()) -> list().
 remove_odd(List) ->
-    remove_odd(List, []).
+	remove_odd(List, []).
 
 -spec remove_odd(list(), list()) -> list().
 remove_odd([], Acc) ->
-    Acc;
+	Acc;
 remove_odd([H|T], Acc) when H rem 2 == 0 ->
-    remove_odd(T, [H|Acc]);
-remove_odd([_H,T], Acc) ->
-    remove_odd(T, Acc).
+	remove_odd(T, [H|Acc]);
+remove_odd([_H|T], Acc) ->
+	remove_odd(T, Acc).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
