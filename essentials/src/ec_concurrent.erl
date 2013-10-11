f3ace64fb8482ef171fe34daf461f6abf4e2510b
Josh Adams <josh@isotope11.com>
2013-10-11 14:40:40 -0500

    Got ec_concurrent working, but I hate nondeterminism

---
+++ essentials/src/ec_concurrent.erl
@@ -0,0 +1,18 @@
+%%% @author Josh Adams
+%%% @doc lol concurrency
+%%% @copyright 2013 PUBLIC DOMAIN GEEBUS
+
+-module(ec_concurrent).
+-export([bf_seq/1]).
+
+%% @doc print a sequence backwards and forward simultaneously
+bf_seq(List) ->
+    RL = lists:reverse(List),
+    spawn(fun() -> print_seq(List) end),
+    spawn(fun() -> print_seq(RL) end).
+
+print_seq([H]) ->
+    io:format("~p", [H]);
+print_seq([H|T]) ->
+    io:format("~p", [H]),
+    print_seq(T).
