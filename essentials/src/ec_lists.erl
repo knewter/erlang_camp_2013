23f8ffd1e229c0048c2c7f0b149906f72e4843c9
Josh Adams <josh@isotope11.com>
2013-10-11 11:55:00 -0500

    Add twomult

--- essentials/src/ec_lists.erl
+++ essentials/src/ec_lists.erl
@@ -3,7 +3,7 @@
 %%% @copyright 2013 PUBLIC DOMAIN GEEBUS
 
 -module(ec_lists).
--export([remove_odd/1]).
+-export([remove_odd/1, twomult/1]).
 
 %% @doc remove odd elements form a list of integers
 -spec remove_odd(list()) -> list().
@@ -14,9 +14,19 @@ remove_odd(List) ->
 remove_odd([], Acc) ->
     Acc;
 remove_odd([H|T], Acc) when H rem 2 == 0 ->
-    remove_odd(T, [H|Acc]);
+    remove_odd(T, Acc ++ [H]);
 remove_odd([_H|T], Acc) ->
     remove_odd(T, Acc).
+
+%% @doc multiply a list of integers by 2
+-spec twomult(list()) -> list().
+twomult(List) ->
+    twomult(List, []).
+
+-spec twomult(list(), list()) -> list().
+twomult([], Acc) -> Acc;
+twomult([H|T], Acc) ->
+    twomult(T, Acc ++ [2*H]).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -28,4 +38,7 @@ remove_odd([_H|T], Acc) ->
 remove_odd_test() ->
     ?assertEqual([], remove_odd([])),
     ?assertEqual([2,4], remove_odd([1,2,3,4,5])).
+
+twomult_test() ->
+    ?assertEqual([2,4], twomult([1,2])).
 -endif.
